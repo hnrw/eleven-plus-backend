1d4dfe980589e911ccb618fcb396df021e59e6f9
Henry <henry@henrywu.co.uk>
2021-06-05 20:01:29 +0100

    add problem model

---
+++ models/problem.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-param-reassign */
+const mongoose = require("mongoose")
+const uniqueValidator = require("mongoose-unique-validator")
+
+const problem = new mongoose.Schema({
+  question: {
+    type: String,
+    unique: true,
+  },
+  correct: {
+    type: String,
+    unique: false,
+  },
+  options: [
+    {
+      type: String,
+    },
+  ],
+})
+
+problem.plugin(uniqueValidator)
+
+problem.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  },
+})
+
+const Problem = mongoose.model("Problem", problem)
+
+module.exports = Problem
