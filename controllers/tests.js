bbf98629ac12ab0c9578129ec24c52ea68a124d1
Henry <henry@henrywu.co.uk>
2021-06-06 11:33:37 +0100

    create tests logic

--- controllers/tests.js
+++ controllers/tests.js
@@ -1,5 +1,7 @@
 const testsRouter = require("express").Router()
 const _ = require("lodash")
+const Test = require("../models/test")
+const verifyUser = require("../helpers/verifyUser")
 
 // const randInt = (max) => Math.floor(Math.random() * max)
 
@@ -40,47 +42,75 @@ const _ = require("lodash")
 //   questions = questions.concat(q)
 // }
 
-const test = [
-  {
-    question: "What is 2 + 3?",
-    multi: true,
-    correct: 5,
-    options: [5, 12, 6, 1],
-  },
-  {
-    question: "What is 1 + 3?",
-    multi: true,
-    correct: 4,
-    options: [19, 2, 18, 4],
-  },
-  {
-    question: "What is 5 + 3?",
-    multi: true,
-    correct: 8,
-    options: [20, 15, 7, 8],
-  },
-  // {
-  //   question: "What is 6 + 3?",
-  //   multi: true,
-  //   correct: 9,
-  //   options: [11, 3, 4, 9],
-  // },
-  // {
-  //   question: "What is 6 + 9?",
-  //   multi: true,
-  //   correct: 15,
-  //   options: [5, 14, 7, 15],
-  // },
-  // {
-  //   question: "What is 6 + 5?",
-  //   multi: true,
-  //   correct: 11,
-  //   options: [16, 4, 2, 11],
-  // },
-]
+// const test = [
+//   {
+//     question: "What is 2 + 3?",
+//     multi: true,
+//     correct: 5,
+//     options: [5, 12, 6, 1],
+//   },
+//   {
+//     question: "What is 1 + 3?",
+//     multi: true,
+//     correct: 4,
+//     options: [19, 2, 18, 4],
+//   },
+//   {
+//     question: "What is 5 + 3?",
+//     multi: true,
+//     correct: 8,
+//     options: [20, 15, 7, 8],
+//   },
+//   // {
+//   //   question: "What is 6 + 3?",
+//   //   multi: true,
+//   //   correct: 9,
+//   //   options: [11, 3, 4, 9],
+//   // },
+//   // {
+//   //   question: "What is 6 + 9?",
+//   //   multi: true,
+//   //   correct: 15,
+//   //   options: [5, 14, 7, 15],
+//   // },
+//   // {
+//   //   question: "What is 6 + 5?",
+//   //   multi: true,
+//   //   correct: 11,
+//   //   options: [16, 4, 2, 11],
+//   // },
+// ]
 
 testsRouter.get("/", async (request, response) => {
+  const tests = await Test.find({})
+  response.send(tests)
+})
+
+testsRouter.get("/:id", async (request, response) => {
+  const id = request.params
+  const test = await Test.findById(id)
   response.send(test)
 })
 
+testsRouter.post("/", async (request, response) => {
+  const { num, problems } = request.body
+  const test = new Test({
+    num,
+    problems,
+  })
+
+  const savedTest = await test.save()
+  response.send(savedTest)
+})
+
+testsRouter.delete("/", async (request, response) => {
+  const admin = await verifyUser(request, response)
+
+  if (admin.email !== "pannicope@gmail.com") {
+    return response.status(400).json({ error: "unauthorized" })
+  }
+  const { id } = request.params
+  await Test.findByIdAndRemove(id)
+  return response.status(204).end()
+})
 module.exports = testsRouter
