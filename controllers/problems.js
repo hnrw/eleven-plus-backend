4e4c06d08bd37fd3a5bfb31ce5a4ef39d94843bf
Henry <henry@henrywu.co.uk>
2021-06-06 11:46:49 +0100

    create problems controller

---
+++ controllers/problems.js
@@ -0,0 +1,33 @@
+const problemsRouter = require("express").Router()
+const Problem = require("../models/problem")
+const verifyUser = require("../helpers/verifyUser")
+
+problemsRouter.get("/", async (request, response) => {
+  const problems = await Problem.find({})
+  response.send(problems)
+})
+
+problemsRouter.post("/", async (request, response) => {
+  const { question, correct, options } = request.body
+  const problem = new Problem({
+    question,
+    correct,
+    options,
+  })
+
+  const savedProblem = await problem.save()
+  response.send(savedProblem)
+})
+
+problemsRouter.delete("/:id", async (request, response) => {
+  const admin = await verifyUser(request, response)
+
+  if (admin.email !== "pannicope@gmail.com") {
+    return response.status(400).json({ error: "unauthorized" })
+  }
+  const { id } = request.params
+  await Problem.findByIdAndRemove(id)
+  return response.status(204).end()
+})
+
+module.exports = problemsRouter
