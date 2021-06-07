58d5da70d691d747e09f02f9bc0d8cd129b90813
Henry <henry@henrywu.co.uk>
2021-06-07 19:50:19 +0100

    fix

--- controllers/gradedTests.js
+++ controllers/gradedTests.js
@@ -18,7 +18,7 @@ gradedTestsRouter.get("/:id", async (request, response) => {
   const { id } = request.params
   const gradedTest = await GradedTest.findById(id)
 
-  if (gradedTest.user.equals(user._id)) {
+  if (!gradedTest.user.equals(user._id)) {
     return response.status(400).send({ error: "unauthorized" })
   }
 
