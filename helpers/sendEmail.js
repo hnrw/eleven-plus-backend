b7f3f45824e271a8e6ad92ba39f7c8234e5625dd
Henry <henry@henrywu.co.uk>
2021-06-10 22:54:39 +0100

    update send email for waterfront

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -3,18 +3,19 @@ const AWS = require("aws-sdk")
 const SES_CONFIG = {
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: "us-east-2",
+  region: "eu-west-2",
 }
 
 const AWS_SES = new AWS.SES(SES_CONFIG)
 
-const passwordReset = (email, resetUrl) => {
+const passwordReset = (data) => {
+  const { email, resetUrl } = data
   // create js object with template data, then convert to json
   const templateData = { url: resetUrl }
   const templateJson = JSON.stringify(templateData)
 
   const params = {
-    Source: "Backstage  < hey@askbackstage.co.uk >",
+    Source: "Waterfront  < admin@waterfrontlearn.com >",
     Template: "BackstagePasswordReset",
     Destination: {
       ToAddresses: [email],
@@ -24,4 +25,6 @@ const passwordReset = (email, resetUrl) => {
   return AWS_SES.sendTemplatedEmail(params).promise()
 }
 
-module.exports = { passwordReset }
+module.exports = {
+  passwordReset,
+}
