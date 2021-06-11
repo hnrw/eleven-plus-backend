7f36a823cbb8adfcab2d9fe0ed796beb73b65474
Henry <henry@henrywu.co.uk>
2021-06-11 10:20:14 +0100

    move subtmit test from tests controller to graded tests

--- controllers/gradedTests.js
+++ controllers/gradedTests.js
@@ -1,6 +1,9 @@
 const gradedTestsRouter = require("express").Router()
 const GradedTest = require("../models/gradedTest")
+const Test = require("../models/test")
+const Problem = require("../models/problem")
 const verifyUser = require("../helpers/verifyUser")
+const answerService = require("../services/answerService")
 
 gradedTestsRouter.get("/", async (request, response) => {
   const user = await verifyUser(request, response)
@@ -23,6 +26,54 @@ gradedTestsRouter.get("/:id", async (request, response) => {
 gradedTestsRouter.get("/all", async (request, response) => {
   const gradedTests = await GradedTest.find({})
   response.send(gradedTests)
+})
+
+gradedTestsRouter.post("/submit", async (request, response) => {
+  const user = await verifyUser(request, response)
+  const { testId, answers } = request.body
+
+  const test = await Test.findById(testId).populate("problems")
+  const problems = await Problem.find({ test: test._id })
+
+  const gradedProblems = problems.map((p) => {
+    const submitted = answers.find((a) => p.equals(a.problemId))
+    const gp = {
+      question: p.question,
+      correct: p.correct,
+      selected: submitted.selected,
+    }
+    return gp
+  })
+
+  const marks = gradedProblems.filter((p) => p.selected === p.correct).length
+  const totalMarks = gradedProblems.length
+  const percent = Math.round((100 / totalMarks) * marks)
+
+  const gradedTest = new GradedTest({
+    test: test._id,
+    user: user._id,
+    marks,
+    total: test.problems.length,
+    num: test.num,
+    percent,
+    gradedProblems,
+    date: Date.now(),
+  })
+
+  const savedGradedTest = await gradedTest.save()
+
+  user.gradedTests = user.gradedTests.concat(savedGradedTest)
+  await user.save()
+
+  answers.forEach((a) => {
+    answerService.createAnswer({
+      user,
+      problemId: a.problemId,
+      selected: a.selected,
+    })
+  })
+
+  response.send(savedGradedTest)
 })
 
 module.exports = gradedTestsRouter
--- controllers/tests.js
+++ controllers/tests.js
@@ -55,53 +55,6 @@ testsRouter.post("/", async (request, response) => {
   response.send(savedTest)
 })
 
-testsRouter.post("/submit", async (request, response) => {
-  const user = await verifyUser(request, response)
-  const { testId, answers } = request.body
-
-  const test = await Test.findById(testId).populate("problems")
-  const problems = await Problem.find({ test: test._id })
-
-  const gradedProblems = problems.map((p) => {
-    const submitted = answers.find((a) => p.equals(a.problemId))
-    const gp = {
-      question: p.question,
-      correct: p.correct,
-      selected: submitted.selected,
-    }
-    return gp
-  })
-
-  const marks = gradedProblems.filter((p) => p.selected === p.correct).length
-  const totalMarks = gradedProblems.length
-  const percent = Math.round((100 / totalMarks) * marks)
-
-  const gradedTest = new GradedTest({
-    test: test._id,
-    user: user._id,
-    marks,
-    total: test.problems.length,
-    num: test.num,
-    percent,
-    gradedProblems,
-    date: Date.now(),
-  })
-
-  const savedGradedTest = await gradedTest.save()
-
-  user.gradedTests = user.gradedTests.concat(savedGradedTest)
-  await user.save()
-
-  answers.forEach((a) => {
-    answerService.createAnswer({
-      user,
-      problemId: a.problemId,
-      selected: a.selected,
-    })
-  })
-
-  response.send(savedGradedTest)
-})
 
 testsRouter.delete("/:id", async (request, response) => {
   const admin = await verifyUser(request, response)
