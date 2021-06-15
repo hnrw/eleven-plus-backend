bc8920ad5782f645a94553a6c75a597e34a1f740
Henry <henry@henrywu.co.uk>
2021-06-15 15:51:39 +0100

    add two tests

---
+++ exams/four.js
@@ -0,0 +1,429 @@
+// MockTests.com Maths 2
+
+const problems = [
+    {
+    question: "Write the following number out in figures",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question:
+      "This is a calculation with a two numbers hidden. What is the value of missing digit A?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "In the calculation above, what is the value of missing digit B?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "What is the sum of the folliwng numbers: 1, 3, 5, 7, 9, 11, 13",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "What is the lowest common multiple of 2, 4, 6 and 8?",
+    correct: "correctcorrectcorrect",
+  {
+    question: "What is the next higher number in the following series?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "Karen spent ⅜ of her money which left her with £240. How much did she have originally?",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "Mrs Hammett asks her class to write fractions that are equivalent to ⅘ on the whiteboard. The fractions the class writes are ⁸⁄₁₀, ³⁄₇, ¹⁶⁄₂₀ and ⁸⁰⁄₁₀₀. Mrs Hammett says one of them is wrong, which one is it?",
+    correct: "correctcorrectcorrect",
+    options: ["⁸⁄₁₀", "³⁄₇", "¹⁶⁄₂₀", "⁸⁰⁄₁₀₀", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "What is the highes common factor of 18 and 30?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "Farmer Joe counted 57 legs in his field. If there are 3 ducks in the field, how many cows are there?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "Which of the following numbers are square numbers?",
+    correct: "correctcorrectcorrect",
+    options: ["35", "6", "15", "26", "64", "11"],
+    multi: true,
+  },
+  {
+    question: "5 apples cost the same as 6 bananas. How many bananas cost as much as 30 apples and 18 bananas?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "6a - 4b + 8c = d. If a = 12, b = 1, and c = 3, what is the value of d?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "A function machine takes a number, divides it by 20 and then takes away 16 from it. The number that comes out is 14. What number went in?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "Sarah, Jane and Lily share out a packet of sweets. Sarah gets 2/7, Jane gets 3/7. How much does Lily get?",
+    correct: "correctcorrectcorrect",
+    options: ["2/7", "4/7", "1/7", "6/7", "3/7"],
+    multi: true,
+  },
+  {
+    question: "Marianne is saving to buy a jacket costing £60, but sees it at 35% off in a sale when she goes to visit her Gran. What does the jacket cost on sale?",
+    correct: "correctcorrectcorrect",
+    unit: "£"
+  },
+  {
+    question: "To make chocolate covered cornflakes, the recipe uses 250g of chocolate to 150g of cornflakes. What is the ratio of chocolate to cornflakes in its simplest form?",
+    correct: "correctcorrectcorrect",
+    options: ["5:3", "10:6", "2:3", "4:7", "1:3"],
+    multi: true,
+  },
+  {
+    question: "Vanya got 40 out of 80 answers right in her science test and 45 out of 100 answers right in her maths test. Did she score better at maths or science?",
+    correct: "correctcorrectcorrect",
+    options: ["Maths", "Science" ],
+    multi: true,
+  },
+  {
+    question: "Four people set out on their bicycles one summer morning. Fred travelled 10 miles in one hour, Hakim travelled 20 miles in one hours, Janey went 6 miles in half an hour and Lily went 14 miles in an hour. What was the average distance travelled by these four in half an hour?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "Mrs Smith is buying uniform trousers for the make staff at SmartMart. What is the median waist size?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "What is the mode of the following ten numbers?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "This week Sally spent £100 on clothes, £27.15 on books, £9.65 on a DVD, £112.31 on a camera and £31.35 on makeup. What is the range in price of the things that Sally bought?",
+    correct: "correctcorrectcorrect",
+    unit: "£"
+  },
+  {
+    question: "What is the probability of picking a vowel at random out of a bag containing the letters M A T H E M A T I C S?",
+    correct: "correctcorrectcorrect",
+    options: ["4/11", "3/7", "5/9 ", "1/2", "5/11"],
+    multi: true,
+  },
+  {
+    question: "The triangle below is an isosceles triangle. If angle c is 96, what are the values of angles a?",
+    correct: "correctcorrectcorrect",
+    unit: "°"
+  },
+  {
+    question: "In the triangle above, what is the value of angle b?",
+    correct: "correctcorrectcorrect",
+    unit: "°"
+  },
+  {
+    question: "Through how many degress does the hour hand of a clock turn between 7.00 and 10am?",
+    correct: "correctcorrectcorrect",
+    unit: "°"
+  },
+  {
+    question: "Bayley is doing an orienteering course. His first directions for the day are to start walking north, when he reaches the first checkpoint he needs to turn 90 degress clockwise and walk in that direction until he reaches the second checkpoint. WHen he gets there he then has to turn 270 degrees anticlockwise. What diection is he now facing?",
+    correct: "correctcorrectcorrect",
+    options: ["North", "South", "East", "West" ],
+    multi: true,
+  },
+  {
+    question: "Which one of the following list of quadrilateral 2D shapes consists of only right angles?",
+    correct: "correctcorrectcorrect",
+    options: ["kite", "rhombus", "square", "parallelogram", "trapezium"],
+    multi: true,
+  },
+  {
+    question: "What is the perimeter of the shape below?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "What is the area of the shape above?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "The diagram below is made up of six squares. If side 'a' in 5cm, what is the area of the shape?",
+    correct: "correctcorrectcorrect",
+    unit: "cm"
+  },
+  {
+    question: "How many edges does a square based pyramid have?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "The school caretaker needs to know how much of the pool chemicals to put the pool, so he neeeds to know what the volume of the pool is. Calculate the volume.",
+    correct: "correctcorrectcorrect",
+    unit: "m³"
+  },
+  {
+    question: "A greengrocer has 72 apples. He would like to seperate the apples into packs of 6 for his customeres. How many packs of apples would he have?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "Which of the following letters does not have a rotational symmetry?",
+    correct: "correctcorrectcorrect",
+    options: ["O", "I", "S", "H", "E"],
+    multi: true,
+  },
+  {
+    question: "My tin of cookies wieghts 3.25kg. If the tin weights 550g, and there are 50 cookies in the ti how much does each cookie weigh?",
+    correct: "correctcorrectcorrect",
+    unit: "g"
+  },
+  {
+    question: "On a city map, the scale is 6cm:1km. If the distance that Harry have to walk is shown on the map as 8cm, how far does Harry actually have to walk?",
+    correct: "correctcorrectcorrect",
+    unit: "km"
+  },
+  {
+    question: "Sohpie spent 45 minutes writing invaitation for her party . She spent a third as long writing the envelopes. How long did she spend altogether?",
+    correct: "correctcorrectcorrect",
+    unit: "minutes",
+  },
+  {
+    question: "A Eurostar ticket from London to Paris costs £82 plus a £1.50 booking charge for each person. How much would it cost for a party of 48 people to travel from London to Paris?",
+    correct: "correctcorrectcorrect",
+    unit: "£"
+  },
+  {
+    question: "Mr Moss bought 480 bricks to build new wall in his garden. He used 5/6 of them. How many does he have left over?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "Jasmin saved £2.50 a week for 12 weeks. She then spends 9% on a new toy. How much does she have left?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "On a school trip, there needs to be one adult for every six children. If fifty six people go on the trip, how many are adults?",
+    multi: true,
+  },
+  {
+    question: "48 children are asked about theiry favourite pets and answers are reocrded on a pie chart. 30° represnts the children who prefer dogs and 90° represents the children whose favourite pets are cats. Half the remaining children chose rabbits and 2 children chose goldfish. Everyone else preferred hamsters. What fraction of the original group preferred hamsters?",
+    correct: "correctcorrectcorrect",
+    options: ["7/24", "8/24", "3/4", "34/64", "5/8"],
+    multi: true,
+  },
+  {
+    question: "A theatre sells 526 tickets for a matinee performance. A school group then books a further 134 tickets. If the theatre hold 1050 people, how many places are left unsold?",
+    correct: "correctcorrectcorrect",
+  },
+  {
+    question: "Hereford Dairies delivers milk every day of the week. If they pay their milkment £a on weekdays and £2a each weekend day, how much does each milkman earn in a week?",
+    correct: "correctcorrectcorrect",
+    unit: "£"
+  },
+  {
+    question: "A standard set of cards has four different suits: hearts, diamond, spades and clubs. What is the probability of choosing a spade from a pack of cards, in its simplest form?",
+    correct: "correctcorrectcorrect",
+    options: ["1/4", "1/2", "3/4", "2/4"],
+    multi: true,
+  },
+  {
+    question: "Matthew goes to the docotor 50 minutes after his Maths lesson finishes and 40 minutes before Music. What time does he leave for the doctors surgery?",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "For Helena's holiday to France, she needs to change some money. For every £1 I exchange, she receives 1.25 Euros. If Helena changes £520, how many Euros will she receive?",
+    correct: "correctcorrectcorrect",
+    unit: "Euros"
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+  {
+    question: "questionquestionquestion",
+    correct: "correctcorrectcorrect",
+    options: ["AAAAAAAA", "BBBBBBB", "CCCCCC", "DDDDDDDDDD", "EEEEEEEE"],
+    multi: true,
+  },
+]
+
+module.exports = problems
