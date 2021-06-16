bcba881840943bf41ca3c87b8059b6904b49fc2b
Henry <henry@henrywu.co.uk>
2021-06-16 12:50:21 +0100

    fix tests

--- exams/three.js
+++ exams/three.js
@@ -2,16 +2,50 @@
 
 const problems = [
   {
-    question:
-      "Which point represents 5.25 on the above number line? Write your asnwer in the space provided above.",
-    correct: "B",
-    options: ["A", "B", "C", "D", "E"],
-    multi: true,
-    img: "https://i.imgur.com/j0SGOw8.png",
+    question: "Calculate 7 x 191",
+    correct: 1337,
   },
   {
-    question: "Round the follwing number to the nearest tenth: 498.36765321",
-    correct: "498.4",
+    question: "Calculate 456 ÷ 12",
+    correct: 38,
+  },
+  {
+    question: "Calculate 987 + 494",
+    correct: 1481,
+  },
+  {
+    question: "Calculate 15²",
+    correct: 225,
+  },
+  {
+    question: "Which number is closest to zero?",
+    correct: "0.053",
+    options: ["0.053", "0.06", "0.1", "0.078", "0.12"],
+    multi: true,
+  },
+  {
+    question:
+      "Which of the following numbers is the lowest Common Multiple (LCM) of 5 and 30?",
+    correct: "30",
+    options: ["5", "10", "6", "30", "3"],
+    multi: true,
+  },
+  {
+    question:
+      "Mr Edward picked 801 of the 7,965 berries on the bushes in his garden. How many are left?",
+    correct: "7164",
+  },
+  {
+    question:
+      "The Glitz Fashion shop was having a sale. Jessica bought five T-shirts for £12.89, four bracelets for £15.73, and six rings for £19.22. How much money did Jessica spend altogether on accessories?",
+    correct: "242.69",
+  },
+  {
+    question:
+      "Which of the following numbers it the remainder when you divide 46 by 6?",
+    correct: "4",
+    options: ["1", "2", "3", "4", "5", "7"],
+    multi: true,
   },
   {
     question:
@@ -19,35 +53,6 @@ const problems = [
     correct: "3",
     options: ["2", "5", "3", "13", "29"],
     multi: true,
-  },
-  {
-    question:
-      "Which of the following numbers is the lowest Common Multiple (LCM) of 5 and 30?",
-    correct: "5",
-    options: ["5", "10", "6", "30", "3"],
-    multi: true,
-  },
-  {
-    question:
-      "Mr Edward picked 801 of the 7,965 berries on the bushes in his garden. How many are left?",
-    correct: "242.69",
-    unit: "£",
-  },
-  {
-    question:
-      "The Glitz Fashion shop was having a sale. Jessica bought five T-shirts for £12.89, four bracelets for £15.73, and six rings for £19.22. How much money did Jessica spend altogether on accessories?",
-    correct: "correctcorrectcorrect",
-  },
-  {
-    question:
-      "Which of the following numbers it her remainder when you divide 46 by 6?",
-    correct: "4",
-    options: ["1", "2", "3", "4", "5", "7"],
-    multi: true,
-  },
-  {
-    question: "Calculate the following sum 456 ÷ 12",
-    correct: "38",
   },
   {
     question:
@@ -75,9 +80,9 @@ const problems = [
     correct: "80.28",
   },
   {
-    question: "Which of the following fractions is closest to ⅕?",
-    correct: "⁶⁄₂₅",
-    options: ["½", "⁵⁄₁₀", "⅗", "⁶⁄₂₅", "⅜"],
+    question: "Which of the following fractions is closest to 1/5?",
+    correct: "6/25",
+    options: ["1/2", "5/10", "3/4", "6/25", "3/8"],
     multi: true,
   },
   {
@@ -90,7 +95,7 @@ const problems = [
   {
     question:
       "What is the new price of a bike which normally costs £75 but it is reduced by 20% in the sale?",
-    correct: "93.75",
+    correct: "60",
     unit: "£",
   },
   {
@@ -102,6 +107,7 @@ const problems = [
       "Which mathematical symbol can be placed in the middle of the fractions below?",
     correct: ">",
     options: [">", "<", "="],
+    img: "https://i.imgur.com/WSXKiOw.png",
     multi: true,
   },
   {
@@ -125,7 +131,7 @@ const problems = [
     correct: "30% of £75",
     options: [
       "50% of £50",
-      "53% of £100",
+      "25% of £100",
       "⅝ of £40",
       "30% of £75",
       "10% of £250",
@@ -135,22 +141,24 @@ const problems = [
   {
     question:
       "Mark has just building a model aeroplane with a scale of 1:72. The model is 20cm long. How long is the actual aeroplane?",
-    correct: 1440,
+    correct: 14.4,
+    unit: "metres",
   },
   {
     question:
-      "During a TV quiz game, the quesitons are answered correctly by presenters Ant and Dec in teh ration of 5:4. If Ant answered 20 questions right, how many questions did Ant Dec answer correctly in total?",
+      "During a TV quiz game, the quesitons are answered correctly by presenters Ant and Dec in the ratio of 5:4. If Ant answered 20 questions right, how many questions did Ant and Dec answer correctly in total?",
     correct: 36,
   },
   {
     question:
       "The graph shows the number of complaint letters received by the local country council over a period of five weeks. What is the range of quantities of letters received?",
     correct: 150,
+    img: "https://i.imgur.com/IwrxYJu.png",
   },
   {
     question:
       "In the graph above, how many more complaint letters were received in week 1 than in week 5?",
-    correct: 150,
+    correct: 100,
   },
   {
     question:
@@ -160,6 +168,7 @@ const problems = [
   {
     question:
       "During the half term holidays, Tina took 7 spelling tests. Here are the results out of 20. What was her mean score?",
+    img: "https://i.imgur.com/24MXc1U.png",
     correct: 15,
   },
   {
@@ -189,20 +198,20 @@ const problems = [
   {
     question: "What is the size of angle 'a' in the diagram?",
     correct: 135,
-    img: "https://i.imgur.com/ao043Y9.png",
+    img: "https://i.imgur.com/eVhNi0t.png",
     unit: "°",
   },
-  {
-    question: "Find the volume of this cuboid in cubic centimetres.",
-    correct: "correctcorrectcorrect",
-    unit: "cm³",
-  },
+  // {
+  //   question: "Find the volume of this cuboid in cubic centimetres.",
+  //   correct: "correctcorrectcorrect",
+  //   unit: "cm³",
+  // },
   {
     question:
       "The diagram shows a square. Two straight lines cut the square into four rectangles. The area of one of the rectangles is shown. Work out the area of the rectangle marked A",
-    correct: "correctcorrectcorrect",
+    correct: 24,
     img: "https://i.imgur.com/ao043Y9.png",
-    unit: "²",
+    unit: "cm²",
   },
   {
     question: "Name the following 2D shape.",
@@ -243,19 +252,44 @@ const problems = [
     unit: "minutes",
   },
 
-  //   {
-  //     question:
-  //       "Which point represents 5.25 on the above number line? Write your asnwer in the space provided above.",
-  //     correct: "correctcorrectcorrect",
-  //     options: [
-  //       "AAAAAAAAAAA",
-  //       "BBBBBBBBBB",
-  //       "CCCCCCCCCC",
-  //       "DDDDDDDDDD",
-  //       "EEEEEEEEE",
-  //     ],
-  //     multi: true,
-  //   },
+  {
+    question:
+      "The pie chart shows the pets belonging to the children in Sue's class. The total number of pets in the survey is 32.\nWhich of the following is the best estimate for the number of dogs owned by the class?",
+    correct: "9",
+    options: ["8", "15", "18", "6", "9"],
+    multi: true,
+    img: "https://i.imgur.com/TpZCE0i.png",
+  },
+  {
+    question:
+      "Look at the information on the right for a breakfast cereal. How much carbohydrate would be in a 20 gram serving of cereal?",
+    correct: "14",
+    unit: "grams",
+    img: "https://i.imgur.com/KNRedQM.png",
+  },
+  {
+    question: "If 349 x 84 = 29 316. What is 349 x 42?",
+    correct: "14 658",
+    options: ["7329", "146 580", "17 264", "58 632", "14 658"],
+    multi: true,
+  },
+  {
+    question:
+      "In the garden, Kylie's father is planting a row of gooseberry bushes. He has alrady planted the first and last bushes in the row and they are 9 metres apart. If gooseberry bushes have to be planted 1.5 metres apart, how many bushes should he plan between the first and last bushes in order to complete the row?",
+    correct: 5,
+  },
+  {
+    question:
+      "If each of these bushes will produce between 14kg and 16kg of fruit next summer, what is the average (mean) weight of fruit he can expect to get from all of his bushes in total?",
+    correct: 105,
+  },
+  {
+    question:
+      "A family's journey to their holiday destination involved a car journey to the airport, a wait of 2 hours, a flight, a wait of one hour and a further car journey to the hotel. Their son, David, worked out that his family had spent three times longer in the aeroplane than on the two car journeys added together. The total journey time was 8 hours from start to finish. How long was the family on the aeroplane?",
+    correct: 75,
+    unit: "minutes",
+  },
 ]
+console.log(problems.length)
 
 module.exports = problems
