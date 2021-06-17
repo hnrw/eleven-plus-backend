2c036530ff1f00926324d8ffb93d04efb79a90cb
Henry <henry@henrywu.co.uk>
2021-06-17 19:24:45 +0100

    remove undeeded controllers

--- services/userService.js
+++ services/userService.js
@@ -8,71 +8,11 @@ const sendEmail = require("../helpers/sendEmail")
 
 const prisma = new PrismaClient()
 
-const getUsers = async (user) => {
+const getUsers = async () => {
   const users = await prisma.user.findMany()
   return {
     status: 200,
     data: users,
-  }
-}
-
-const getProfile = async (id) => {
-  const user = await User.findById(id)
-  // .populate({
-  //   path: "getQuestions",
-  //   select: { content: 1, date: 1, answer: 1, fromUser: 1, likedBy: 1 },
-  //   populate: [
-  //     {
-  //       path: "answer",
-  //       select: { content: 1, date: 1 },
-  //     },
-  //     {
-  //       path: "fromUser",
-  //       select: { name: 1, username: 1, profilePicture: 1 },
-  //     },
-  //     {
-  //       path: "toUser",
-  //       select: { name: 1, username: 1, profilePicture: 1 },
-  //     },
-  //   ],
-  // })
-  // .populate("sentQuestions", {
-  //   content: 1,
-  //   date: 1,
-  // })
-  // .populate("answers", {
-  //   content: 1,
-  //   date: 1,
-  // })
-
-  // don't return private info
-  user.email = undefined
-  user.dob = undefined
-  user.gender = undefined
-
-  return {
-    status: 200,
-    data: user,
-  }
-}
-
-const searchUsers = async (searchParam) => {
-  const users = await User.find({})
-
-  const options = {
-    minMatchCharLength: 3,
-    keys: ["name", "parentName"],
-  }
-
-  const fuse = new Fuse(users, options)
-
-  const results = fuse.search(searchParam)
-  // fuse returns an object of item and refIndex. We only need item, which contains the user obj
-  const usersResult = results.map((result) => result.item)
-
-  return {
-    status: 200,
-    data: usersResult,
   }
 }
 
@@ -138,40 +78,40 @@ const createUser = async (data) => {
   }
 }
 
-const editUser = async (user, data) => {
-  const fieldToUpdate = {
-    firstName: data.firstName,
-    lastName: data.lastName,
-    dob: data.dob,
-    gender: data.gender,
-  }
+// const editUser = async (user, data) => {
+//   const fieldToUpdate = {
+//     firstName: data.firstName,
+//     lastName: data.lastName,
+//     dob: data.dob,
+//     gender: data.gender,
+//   }
 
-  Object.keys(fieldToUpdate).forEach((key) => {
-    const value = fieldToUpdate[key]
-    if (!value) {
-      delete fieldToUpdate[key]
-    }
-  })
+//   Object.keys(fieldToUpdate).forEach((key) => {
+//     const value = fieldToUpdate[key]
+//     if (!value) {
+//       delete fieldToUpdate[key]
+//     }
+//   })
 
-  const savedUser = await User.findByIdAndUpdate(
-    user.id,
-    { $set: { ...fieldToUpdate } },
-    {
-      runValidators: true,
-      new: true,
-    }
-  )
+//   const savedUser = await User.findByIdAndUpdate(
+//     user.id,
+//     { $set: { ...fieldToUpdate } },
+//     {
+//       runValidators: true,
+//       new: true,
+//     }
+//   )
 
-  return {
-    status: 200,
-    data: savedUser,
-  }
-}
+//   return {
+//     status: 200,
+//     data: savedUser,
+//   }
+// }
 
 module.exports = {
   getUsers,
   createUser,
-  editUser,
-  getProfile,
-  searchUsers,
+  // editUser,
+  // getProfile,
+  // searchUsers,
 }
