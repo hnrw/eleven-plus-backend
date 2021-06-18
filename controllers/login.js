a471d4b91e241b269474bac164226a2ad8ccf3a0
Henry <henry@henrywu.co.uk>
2021-06-18 11:30:46 +0100

    login with prisma

--- controllers/login.js
+++ controllers/login.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken")
+const { PrismaClient } = require("@prisma/client")
 const bcrypt = require("bcrypt")
 const loginRouter = require("express").Router()
 const User = require("../models/user")
 
+const prisma = new PrismaClient()
 loginRouter.post("/", async (request, response) => {
   const { email, password } = request.body
-  const user = await User.findOne({ email })
+  const user = await prisma.user.findUnique({ where: { email } })
 
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash)
