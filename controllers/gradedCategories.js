96f94df443477d0772d31044c2c1cea6d9c8f2dc
Henry <henry@henrywu.co.uk>
2021-06-23 09:46:56 +0100

    calculate average score across each category

--- controllers/gradedCategories.js
+++ controllers/gradedCategories.js
@@ -16,4 +16,29 @@ gradedCategoriesRouter.get("/", async (req, res) => {
   res.send(gradedCategories)
 })
 
+gradedCategoriesRouter.get("/average", async (req, res) => {
+  const gradedCategories = await prisma.gradedCategory.findMany({
+    where: {
+      attempts: {
+        gt: 0,
+      },
+    },
+  })
+
+  const categories = await prisma.category.findMany()
+
+  const categoriesAveraged = categories.map((c) => {
+    const mgc = gradedCategories.filter((gc) => gc.categoryName === c.name)
+
+    const totalAttempts = mgc.reduce((acc, gc) => gc.attempts + acc, 0)
+    const totalCorrect = mgc.reduce((acc, gc) => gc.correct + acc, 0)
+    return {
+      average: (100 * totalCorrect) / totalAttempts,
+      name: c.name,
+    }
+  })
+
+  res.send(categoriesAveraged)
+})
+
 module.exports = gradedCategoriesRouter
